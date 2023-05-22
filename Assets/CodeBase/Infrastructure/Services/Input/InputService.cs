32aeff257c94543a0174dc1e4bd31b08dd1e3ff2
Maxumillian <maximillian.sh23@gamil.com>
2023-05-22 17:52:52 +0300

    Addet input service based on simple input

---
+++ Assets/CodeBase/Infrastructure/Services/Input/InputService.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Infrastructure.Services.Input
+{
+    public abstract class InputService : IInputService
+    {
+        protected const string HorizontalAxisName = "Horizontal";
+        protected const string VerticalAxisName = "Vertical";
+        private const string AttackButtonName = "Fire";
+
+        public abstract Vector2 Axis { get; } 
+        
+        public bool IsAttackButtonPressed() =>
+            SimpleInput.GetButtonUp(AttackButtonName);
+
+        protected static Vector2 SimpleInputAxis() => 
+            new Vector2(SimpleInput.GetAxis(HorizontalAxisName), SimpleInput.GetAxis(VerticalAxisName));
+    }
+}
